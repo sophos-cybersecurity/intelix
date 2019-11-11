b3d1e2980e9fefa1a525a9b31a1ea8d962c53a59
Stephen O'Leary <stephen.oleary@sophos.com>
2019-11-11 15:59:59 +0000

    modified setup.py for PyPi to take readme file as long description

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='intelix',
-      version='0.1',
+      version='0.1.1',
       description='Sophos Labs Intelix Client',
       author='Stephen O''Leary',
       author_email='stephen.oleary@sophos.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       packages=find_packages())
